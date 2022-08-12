93d4229bfeb4df1b17b5838009ec61d0b55fd09d
Mo'men Sherif <momensherif.2019@gmail.com>
2022-08-12 13:44:51 +0200

    Add jest * jsdom

--- Basics/src/math.test.js
+++ Basics/src/math.test.js
@@ -17,22 +17,3 @@ test('subtract 4 - 2 to be 2', () => {
   expect(result).toBe(expected);
 });
 
-async function test(title, callback) {
-  try {
-    await callback();
-    console.log(`✅ ${title}`);
-  } catch (error) {
-    console.log(`❌ ${title}`);
-    console.error(`${error}`);
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    },
-  };
-}
