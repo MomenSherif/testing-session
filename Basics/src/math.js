cc7449b8f4c74f43d8444bb7a171f066521ad704
Mo'men Sherif <momensherif.2019@gmail.com>
2022-08-12 12:00:10 +0200

    handle async tests

--- Basics/src/math.js
+++ Basics/src/math.js
@@ -1,7 +1,11 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
+const sumAsync = (a, b) =>
+  new Promise(res => setTimeout(() => res(a + b), 2000));
+
 module.exports = {
   sum,
   subtract,
+  sumAsync,
 };
--- Basics/src/math.test.js
+++ Basics/src/math.test.js
@@ -1,9 +1,14 @@
-const { sum, subtract } = require('./math');
+const { sum, subtract, sumAsync } = require('./math');
 
 test('sum 1 + 2 to be 3', () => {
   const result = sum(1, 3);
   const expected = 4;
   expect(result).toBe(expected);
+});
+
+test('sumAsync 1 + 2 to resolve 3', async () => {
+  const result = await sumAsync(1, 2);
+  expect(result).toBe(3);
 });
 
 test('subtract 4 - 2 to be 2', () => {
@@ -12,9 +17,9 @@ test('subtract 4 - 2 to be 2', () => {
   expect(result).toBe(expected);
 });
 
-function test(title, callback) {
+async function test(title, callback) {
   try {
-    callback();
+    await callback();
     console.log(`✅ ${title}`);
   } catch (error) {
     console.log(`❌ ${title}`);
