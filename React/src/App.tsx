d23c6311485cc45b657e977d581db2648c10d4be
Mo'men Sherif <momensherif.2019@gmail.com>
2022-08-13 13:13:01 +0200

    Add basic counter example

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './01-basics/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +16,7 @@ function App() {
         >
           Learn React
         </a>
+        <Counter />
       </header>
     </div>
   );
