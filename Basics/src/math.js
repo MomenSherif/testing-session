b7590c37138183b64edd7e0e47edb7420d1e665b
Mo'men Sherif <momensherif.2019@gmail.com>
2022-08-18 09:48:28 +0200

    remove code samples

--- Basics/src/math.js
+++ Basics/src/math.js
@@ -1,5 +1,5 @@
-export const sum = (a, b) => a + b;
-export const subtract = (a, b) => a - b;
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 
-export const sumAsync = (a, b) =>
+const sumAsync = (a, b) =>
   new Promise(res => setTimeout(() => res(a + b), 4000));
--- Basics/src/mocking/__mocks__/utils.ts
+++
@@ -1,1 +0,0 @@
-export const getWinner = jest.fn(() => '__mocks__');
